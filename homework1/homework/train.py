9c2ee70e521ca699d3c103328228c3388e196b6b
Seju Bidiwala <sbbidiwala@gmail.com>
2021-06-27 17:26:59 -0500

    hw1: First stab at training code.

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -1,15 +1,29 @@
 from .models import ClassificationLoss, model_factory, save_model
 from .utils import accuracy, load_data
-
+import torch
 
 def train(args):
+    n_epochs = 100
+    batch_size = 128
+
+    train_data = load_data('data/train', batch_size=batch_size)
+    print('Length of data: ', len(train_data))
+    valid_data = load_data('data/valid')
+  
     model = model_factory[args.model]()
+    loss_fn = ClassificationLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
 
-    """
-    Your code here
+    for epoch in range(n_epochs):
+      for batch_num in range(len(train_data)):
+        train_features, train_labels = next(iter(train_data))
 
-    """
-    raise NotImplementedError('train')
+        output = model(train_features)
+        loss = loss_fn(output, train_labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
     save_model(model)
 
